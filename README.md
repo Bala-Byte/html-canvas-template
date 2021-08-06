b037f3460b25470c8de118dac3b75f18aaf9ee31
Balázs Barna <barna.balazs66@gmail.com>
2021-08-06 22:44:55 +0200

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Template
 
+Some code to start the set up process of any canvas involved project faster
+
+Includes:
+-- Basic Gravity
--- index.js
+++ index.js
@@ -1,20 +1,19 @@
-//setup
+// Setup
 var canvas = document.querySelector('canvas');
 var c = canvas.getContext('2d');
 
 canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
 
-//Fun part
+// Fun part
 var mouse = {
      x: undefined,
      y: undefined,
      alt: false
 };
 
-//settings
-var circleCount = Math.round(innerHeight / 30);
-var bouncyNess = true;
+// Settings
+const circleCount = 1; //Math.round(innerHeight / 30);
 
 window.addEventListener('mousemove', function (event) {
      mouse.x = event.x;
@@ -28,16 +27,19 @@ window.addEventListener('resize', function () {
 });
 
 function Circle(x, y, dx, dy, radius) {
-     this.bounce = 1;
+     this.bounce = true;
      this.x = x;
      this.y = y;
-     this.startX = x;
-     this.startY = y;
+     //this.startX = x;
+     //this.startY = y;
      this.dx = dx;
      this.dy = dy;
-     this.gravity = 0;
+     this.gravity = .981;
+     this.weight = 0; // 0 - Normal; 1 - Extra heavy
      this.radius = radius;
      this.color = `rgb(${Math.random() * 225 + 1}, ${Math.random() * 225 + 1}, ${Math.random() * 225 + 1})`;
+
+     this.gravity += this.weight;
 
      this.draw = function () {
           c.beginPath();
@@ -46,71 +48,63 @@ function Circle(x, y, dx, dy, radius) {
           c.fill();
           c.strokeStyle = this.color;
           c.stroke();
-     };
+     }
 
      this.update = function () {
-          //X Side bounce
+          // X Side bounce
           if (this.x + this.radius > innerWidth || this.x - this.radius < 0) {
 
-               //slowly reset x movement
-               this.dx -= 0.1;
+               // Slowly decrease x movement
+               //this.dx -= 0.5;
 
-               //bounce
+               // Bounce
                this.dx = -this.dx;
-          };
+          }
 
-          //Y up-down bounce
-          if (bouncyNess) {
-               if (this.y + this.radius > innerHeight || this.y - this.radius < 0) {
+          // Y up-down bounce
+          if (this.bounce) {
+               
+               if (this.y + this.radius > innerHeight /*|| this.y - this.radius < 0*/) {
 
-                    //this.dy = -this.dy;
-                    this.gravity = -this.gravity;
-                    //this.startY += 10;
+                    // Sinking
+                    if(this.dy > 0){
+                         this.gravity += this.weight;
+                         this.dy = -this.dy;
+                    }else {
+                         this.dy = 0;
+                         this.bounce = false;
+                    }
                }
-          };
+          }
 
-          //TEST
-          if (mouse.alt) {
+          // Check needed to check changes in previous if>if>if-else (this.bounce = false)
+          if (this.bounce)
+               this.dy += this.gravity;
+
+          // TEST
+          /*if (mouse.alt) {
                this.x = mouse.x;
                this.y = mouse.y;
-          };
-
-          //gravity increase
-          if (this.y + this.radius > innerHeight + this.radius) {
-               this.dy = 0;
-               this.gravity = 0;
-               this.y = innerHeight - this.radius;
-               //console.log('end');
-               this.bounce = 0;
-          };
-
-          if (this.bounce == 1) {
-               this.gravity += 1;
-               //console.log(this.gravity);
-          };
-
+          }*/
 
           this.x += this.dx;
-          //this.y += this.dy;
-
-          //gravity
-          this.y += this.gravity;
+          this.y += this.dy;
 
           this.draw();
-     };
+     }
 };
 
 var circles = [];
 
-for (var i = 0; i < circleCount + 1; i++) {
-
-     var x = Math.round(Math.random() * (innerWidth - radius * 2) + radius);
-     var y = Math.round(Math.random() * (innerHeight - radius * 2));
-     var dx = Math.round((Math.random() - 0.5) * 10);
-     var dy = Math.round((Math.random() - 0.5) * 10);
-     var radius = Math.random() + 30;
-
-     circles.push(new Circle(x, y, dx, dy, radius));
+for (var i = 0; i < circleCount; i++) {
+     var radius = 30;
+     circles.push(new Circle(
+          Math.round(Math.random() * (innerWidth - radius * 2) + radius),
+          Math.round(Math.random() * (innerHeight - radius * 2)),
+          Math.round((Math.random() - 0.5) * 10),
+          Math.round((Math.random() - 0.5) * 10),
+          radius
+     ));
 };
 
 function animate() {
