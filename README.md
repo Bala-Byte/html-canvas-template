490e7d8f6934e7ae516804eea1789e09637b03c3
Balázs Barna <barna.balazs66@gmail.com>
2021-08-10 13:29:28 +0200

    Initial Commit

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 Includes:
 -- Basic Gravity
+-- Movable Emitters
+-- TODO: Multiple Emitters
+-- TODO: Motion Blur
+-- TODO: Image as Particle background
--- index.js
+++ index.js
@@ -5,20 +5,23 @@ var c = canvas.getContext('2d');
 canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
 
-// Fun part
-var mouse = {
+/*var mouse = {
      x: undefined,
      y: undefined,
      alt: false
-};
+};*/
 
 // Settings
-const circleCount = 1; //Math.round(innerHeight / 30);
+var circlePerFrame = 1;
+var emitterLocation = {
+     x: 500,
+     y: 100
+};
 
-window.addEventListener('mousemove', function (event) {
-     mouse.x = event.x;
-     mouse.y = event.y;
-     mouse.alt = event.altKey
+window.addEventListener('click', function (event) {
+     emitterLocation.x = event.x;
+     emitterLocation.y = event.y;
+     //mouse.alt = event.altKey
 });
 
 window.addEventListener('resize', function () {
@@ -26,7 +29,7 @@ window.addEventListener('resize', function () {
      canvas.width = window.innerWidth;
 });
 
-function Circle(x, y, dx, dy, radius) {
+function Circle(x, y, dx, dy, radius, lifetime) {
      this.bounce = true;
      this.x = x;
      this.y = y;
@@ -35,22 +38,27 @@ function Circle(x, y, dx, dy, radius) {
      this.dx = dx;
      this.dy = dy;
      this.gravity = .981;
-     this.weight = 0; // 0 - Normal; 1 - Extra heavy
+     this.weight = 1; // 1 - Normal; 0 - Weightless; -1 - Minus Weight
      this.radius = radius;
+     this.lifetime = lifetime * 60;
      this.color = `rgb(${Math.random() * 225 + 1}, ${Math.random() * 225 + 1}, ${Math.random() * 225 + 1})`;
 
-     this.gravity += this.weight;
+     this.gravity *= this.weight;
 
      this.draw = function () {
           c.beginPath();
           c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+          c.globalAlpha = this.lifetime / (lifetime * 60);
           c.fillStyle = this.color;
           c.fill();
-          c.strokeStyle = this.color;
-          c.stroke();
+          //c.strokeStyle = this.color;
+          //c.stroke();
      }
 
      this.update = function () {
+          if (this.lifetime)
+               this.lifetime--;
+
           // X Side bounce
           if (this.x + this.radius > innerWidth || this.x - this.radius < 0) {
 
@@ -63,14 +71,14 @@ function Circle(x, y, dx, dy, radius) {
 
           // Y up-down bounce
           if (this.bounce) {
-               
+
                if (this.y + this.radius > innerHeight /*|| this.y - this.radius < 0*/) {
 
                     // Sinking
-                    if(this.dy > 0){
+                    if (this.dy > 0) {
                          this.gravity += this.weight;
                          this.dy = -this.dy;
-                    }else {
+                    } else {
                          this.dy = 0;
                          this.bounce = false;
                     }
@@ -81,12 +89,6 @@ function Circle(x, y, dx, dy, radius) {
           if (this.bounce)
                this.dy += this.gravity;
 
-          // TEST
-          /*if (mouse.alt) {
-               this.x = mouse.x;
-               this.y = mouse.y;
-          }*/
-
           this.x += this.dx;
           this.y += this.dy;
 
@@ -96,24 +98,34 @@ function Circle(x, y, dx, dy, radius) {
 
 var circles = [];
 
-for (var i = 0; i < circleCount; i++) {
-     var radius = 30;
-     circles.push(new Circle(
-          Math.round(Math.random() * (innerWidth - radius * 2) + radius),
-          Math.round(Math.random() * (innerHeight - radius * 2)),
-          Math.round((Math.random() - 0.5) * 10),
-          Math.round((Math.random() - 0.5) * 10),
-          radius
-     ));
-};
-
 function animate() {
      requestAnimationFrame(animate);
      c.clearRect(0, 0, innerWidth, innerHeight);
 
-     for (const circle of circles) {
-          circle.update();
-     }
+     for (var i = 0; i < circlePerFrame; i++) {
+          var radius = 10;
+          circles.push(new Circle(
+               emitterLocation.x,
+               emitterLocation.y,
+               Math.round((Math.random() - 0.5) * 10),
+               Math.round((Math.random() - 0.5) * 10),
+               radius,
+               .6
+          ));
+     };
+
+     c.beginPath();
+     c.arc(emitterLocation.x, emitterLocation.y, 30, 0, Math.PI * 2, false);
+     c.fillStyle = "white";
+     c.fill();
+
+     //c.globalCompositeOperation = "lighten";
+
+     for (var i = 0; i < circles.length; i++)
+          if (circles[i].lifetime == 0)
+               circles.splice(i, 1);
+          else
+               circles[i].update();
 };
-console.log('Circle generated: ' + circleCount)
+//console.log('Circle generated: ' + circlePerFrame);
 animate();
